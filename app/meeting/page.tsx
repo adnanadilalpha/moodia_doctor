b3bf09bc6e328e29c0f00c196d5e4bd6002cb305
Adnan Adil <124282322+adnanadilalpha@users.noreply.github.com>
2024-10-06 00:42:35 +0500

    meeting page fixed build issue

--- app/meeting/page.tsx
+++ app/meeting/page.tsx
@@ -1,61 +1,10 @@
-"use client";
+import React, { Suspense } from "react";
+import DoctorMeeting from "../components/meeting";
 
-import { useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { FaPhoneSlash } from "react-icons/fa";
-import { useRouter } from "next/navigation";
-
-const DoctorMeeting: React.FC = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const [loading, setLoading] = useState(true);
-
-  // Get the 'url' query parameter and decode it
-  const meetingUrl = decodeURIComponent(searchParams.get("url") || "");
-
-  useEffect(() => {
-    if (!meetingUrl) {
-      alert("Meeting URL is missing");
-      router.back(); // Redirect back if there's no meeting URL
-    }
-  }, [meetingUrl, router]);
-
-  const handleEndMeeting = () => {
-    // End meeting and navigate back to the previous page or a specific route
-    router.back();
-  };
-
+export default function MeetingPage() {
   return (
-    <div className="flex flex-col items-center h-screen">
-      {/* Embed the decoded Daily.co meeting URL using iframe */}
-      {meetingUrl ? (
-        <iframe
-          src={meetingUrl}
-          allow="camera; microphone; fullscreen; speaker; display-capture"
-          className="w-full h-full"
-          onLoad={() => setLoading(false)}
-          title="Daily.co Video Call"
-        ></iframe>
-      ) : (
-        <p>Invalid meeting URL.</p>
-      )}
-
-      {loading && (
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <p className="text-white">Loading meeting...</p>
-        </div>
-      )}
-
-      <div className="flex justify-center mt-4 fixed bottom-4">
-        <button
-          onClick={handleEndMeeting}
-          className="p-2 rounded-full bg-red-500 text-white"
-        >
-          <FaPhoneSlash className="text-xl" />
-        </button>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading meeting page...</div>}>
+      <DoctorMeeting />
+    </Suspense>
   );
-};
-
-export default DoctorMeeting;
+}
